1440d7ef7701cb85767d0d188bb56e1de200d0f8
Brandon Delgado <TheBrandonDelgado@gmail.com>
2023-09-16 05:52:04 -0500

    Add project

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './code/main.js',
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader","css-loader"]
+          },
+          {
+            test: /\.png$/i,
+            type: "asset/resource"
+          },
+          {
+            test: /\.txt$/i,
+            type: 'asset/source'
+          },
+          {
+            test: /\.ttf$/i,
+            type: "asset/resource"
+          }
+        ]
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist/',
+  },
+  devServer: {
+      static: {
+          directory: path.join(__dirname, '/')
+      }
+  },
+};
